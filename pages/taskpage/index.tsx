9ffdf69648152b48f706fc1d9f29bc7da4c2e536
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-03-31 16:59:05 +0200

    Added DisplayTasks To /taskpage/index

--- pages/taskpage/index.tsx
+++ pages/taskpage/index.tsx
@@ -1,9 +1,15 @@
+import DisplayTasks from "@/components/displayTask"
+import tempGetTaskData from "@/components/tempGetTaskData"
 import { NextPage } from "next"
 
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
-  return <div>Tasks</div>
+  return (
+    <div>
+      <DisplayTasks data={tempGetTaskData()}></DisplayTasks>
+    </div>
+  )
 }
 
 export default Index
