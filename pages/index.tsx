065ca31547d40ed050315c690b006ff9f4f5b6a0
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-03-31 15:18:07 +0200

    Finished adding basic today's Tasks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next"
+import DisplayTasks from "@/components/displayTask"
+import { useState } from "react"
+import tempGetTaskData from "@/components/tempGetTaskData"
 
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
-  return <div>Index</div>
+  return (
+    <div>
+      <DisplayTasks data={tempGetTaskData()}></DisplayTasks>
+    </div>
+  )
 }
 
 export default Index
