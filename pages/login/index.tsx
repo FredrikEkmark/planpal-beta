f1bc6c055a5eff885c800cc6a7343adb1ad359e4
Supra39 <112933696+Supra39@users.noreply.github.com>
2023-04-05 13:52:46 +0200

    erik/konrad addad logo + image, loginPage

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next"
 import * as React from "react"
-import Avatar from "@mui/material/Avatar"
 import Button from "@mui/material/Button"
 import CssBaseline from "@mui/material/CssBaseline"
 import TextField from "@mui/material/TextField"
@@ -9,10 +8,10 @@ import Checkbox from "@mui/material/Checkbox"
 import Link from "@mui/material/Link"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Image from "next/image"
 
 const theme = createTheme()
 
@@ -38,12 +37,30 @@ export default function SignIn() {
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+          <div className="logo">
+            <Image
+              src={"/Logo.svg"}
+              alt={"#"}
+              width={"200"}
+              height={"200"}
+            ></Image>
+          </div>
+
+          <div className="slogan">
+            <Typography component="h1" variant="h5">
+              Planning made easy!
+            </Typography>
+          </div>
+
+          <div className="purpleImage">
+            <Image
+              src={"/Purple symbol.svg"}
+              alt={"#"}
+              width={"99"}
+              height={"97"}
+            ></Image>
+          </div>
+
           <Box
             component="form"
             onSubmit={handleSubmit}
