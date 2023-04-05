f0a246ad1908ca4bbc8dcaecc5f2c16f8450ae12
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-04-05 15:31:57 +0200

    colors added in extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-
+        ourcolors: {
+          purple: "#b577d5",
+          green: "#6ABEA6",
+          yellow: "#FBEE7B",
+          white: "#F3F3F3",
+          font: "#646464",
+        },
 
       },
     },
